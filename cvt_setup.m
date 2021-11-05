57d96cfb8dc7670b27eebf18750f8bb95936b838
Andrea <andrea@Sacripante.local>
2021-11-05 15:48:32 +0100

    first commit

---
+++ cvt_setup.m
@@ -0,0 +1,13 @@
+
+%restoredefaultpath;
+
+root  = pwd;
+addpath(fullfile(root,'m-files'             ));
+addpath(fullfile(root,'m-files','aux_fun'       ));
+addpath(fullfile(root,'thirdparty'          ),'-end');
+
+savepath;
+
+help m-files
+
+
